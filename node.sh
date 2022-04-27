8031cab6fa00f4cfa13a57e58bf4ca66152cdb7b
alex.buckton <alex.buckton@healius.com.au>
2022-04-27 13:00:56 +1000

    up

--- node.sh
+++ node.sh
@@ -81,6 +81,8 @@ fi
 echo Turning on SSL...
 sed -i 's/dbms.connector.https.enabled=false/dbms.connector.https.enabled=true/g' /etc/neo4j/neo4j.conf
 
+sed -i 's/#dbms.routing.enabled=false/dbms.routing.enabled=true/g' /etc/neo4j/neo4j.conf
+
 #### Todo - Any reason we're not running this line?
 #sed -i 's/#dbms.connector.bolt.tls_level=DISABLED/dbms.connector.bolt.tls_level=OPTIONAL/g' /etc/neo4j/neo4j.conf
 
